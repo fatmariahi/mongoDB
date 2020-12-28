2dfca80137f0b9bcb3ef2071a83047965cfeb3a1
fatmariahi <riahi.fatma.r@gmail.com>
2020-12-28 21:17:11 +0100

    initial Commit: test git

--- src/main/java/com/example/mongoDB/MongoDbApplication.java
+++ src/main/java/com/example/mongoDB/MongoDbApplication.java
@@ -1,9 +1,11 @@
 package com.example.mongoDB;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableSwagger2
 public class MongoDbApplication {
 
 	public static void main(String[] args) {
